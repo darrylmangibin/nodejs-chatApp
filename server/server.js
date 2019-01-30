557ea93533c47ed3c8a24f7f1be167878d990e93
Darryl <darrylmangibin@gmail.com>
2019-01-30 23:38:52 +0800

    new user and alert ohter user

--- server/server.js
+++ server/server.js
@@ -14,12 +14,27 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected')
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome'
+    })
+
+    socket.broadcast.emit('newMessage', {
+        from:'Admin',
+        text: 'new user found'
+    })
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message)
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
+        socket.broadcast.emit('newMessage', {
+                from: message.from,
+                text: message.text,
+                createdAt: new Date().getTime()
         })
     })
 
