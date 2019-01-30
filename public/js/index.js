d19d6e2b63390b687af3a69518ece06796326329
Darryl <darrylmangibin@gmail.com>
2019-01-31 02:06:17 +0800

    add css

--- public/js/index.js
+++ public/js/index.js
@@ -30,11 +30,12 @@ socket.on('newMessage', function(message) {
 $(function() {
     $('#message-form').on('submit', function(e) {
         e.preventDefault()
+        var messageTextBox = $('[name=message]')
         socket.emit('createMessage', {
             name: 'User',
-            text: $('[name=message]').val()
+            text: messageTextBox.val()
         }, function() {
-
+            messageTextBox.val('');
         })
     })
 
@@ -43,13 +44,18 @@ $(function() {
         if(!navigator.geolocation) {
             return alert('Geolocation not supported by your browser')
         }
+
+        locationButton.attr('disabled', 'disabled').text('Sending location...')
+
         navigator.geolocation.getCurrentPosition(function(position) {
+            locationButton.removeAttr('disabled').text('Send Location')
             socket.emit('createLocationMessage', {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             });
 
         }, function() {
+            locationButton.removeAttr('disabled').text('Send Location').text('Send Location')
             alert('Unable to fetch location')
         })
     })
